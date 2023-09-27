96e029b2fa7827211b8d6c43b8bee9edbd787b31
Felipe Díaz Garay <f.diazgaray@gmail.com>
2023-09-27 18:53:14 -0300

    [FIX]Corrección META descripción

--- js/contacto.js
+++ js/contacto.js
@@ -25,33 +25,3 @@ function llamadaPaises() {
 llamadaPaises();
 
 
-//? Fecha actual
-
-
-// Obtener el elemento de entrada de fecha de inicio y final
-const fechaInicio = document.querySelector("#dateMix");
-const fechaFin = document.querySelector("#dateMaster");
-
-// Establecer la fecha mínima en el campo de entrada de fecha de inicio
-const fechaActual = new Date();
-const fechaActualISO = fechaActual.toISOString().split("T")[0];
-fechaInicio.setAttribute("min", fechaActualISO);
-
-// Agregar un controlador de eventos change al campo de entrada de fecha de inicio
-fechaInicio.addEventListener("change", function () {
-    // Obtener la fecha seleccionada en el campo de entrada de fecha de inicio
-    const fechaInicioSeleccionada = new Date(this.value);
-
-    // Establecer la fecha mínima en el campo de entrada de fecha de final
-    const fechaInicioISO = fechaInicioSeleccionada.toISOString().split("T")[0];
-    fechaFin.setAttribute("min", fechaInicioISO);
-
-    if (fechaInicio.value === '') {
-        fechaFin.disabled = true;
-    } else {
-        fechaFin.disabled = false;
-    }
-
-});
-
-
